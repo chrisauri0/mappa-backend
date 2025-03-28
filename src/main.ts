35bfabf1b4be17da6f505f95e83d459b4664405a
chrisauri0 <chaparitoveb20@gmail.com>
2025-03-28 08:56:05 -0600

    subiendo a vercel

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as serverless from 'serverless-http';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.enableCors({
-    origin: 'http://localhost:3000', // Origen permitido
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
-    credentials: true, // Permitir envío de cookies
+    origin: '*', // Ajusta según sea necesario
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.init();
+
+  if (process.env.NEST_ENV === 'vercel') {
+    module.exports = serverless(server);
+  } else {
+    await app.listen(process.env.PORT ?? 5000);
+  }
 }
+
 bootstrap();
