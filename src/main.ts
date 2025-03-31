e42f21faec8414d543b0e81ff6ed6b71b352b570
chrisauri0 <chaparitoveb20@gmail.com>
2025-03-30 22:03:22 -0600

    subiendo a railway

--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
-import * as serverless from 'serverless-http';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-  app.enableCors({
-    origin: '*', // Ajusta según sea necesario
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
 
   // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('API de Mi Proyecto')
     .setDescription('Documentación de la API con Swagger')
     .setVersion('1.0')
-    .addTag('Endpoints')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('/docs', app, document);
 
   await app.init();
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT || 5000, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT || 5000}`);
+  });
 }
 
 bootstrap();
