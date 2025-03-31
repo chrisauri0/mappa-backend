f0b1f032b39ac7460064d340205dee61f415ca7b
chrisauri0 <chaparitoveb20@gmail.com>
2025-03-30 21:41:39 -0600

    subiendo a rainwail

--- src/main.ts
+++ src/main.ts
@@ -16,33 +16,7 @@ async function bootstrap() {
 
   await app.init();
 
-  // Endpoint de depuración de rutas (sólo en desarrollo)
-  const httpAdapter = app.getHttpAdapter();
-  const expressApp = httpAdapter.getInstance(); // Instancia de Express
-
-  expressApp.get('/debug-routes', (req, res) => {
-    const router = expressApp._router;
-
-    if (!router) {
-      return res.status(500).json({ error: 'Router no disponible' });
-    }
-
-    const routes = router.stack
-      .filter((layer) => layer.route)
-      .map((layer) => ({
-        method: Object.keys(layer.route.methods)[0].toUpperCase(),
-        path: layer.route.path,
-      }));
-
-    res.json(routes);
-  });
-
-  // Para despliegue en Vercel
-  if (process.env.NEST_ENV === 'vercel') {
-    module.exports = serverless(server);
-  } else {
-    await app.listen(process.env.PORT ?? 5000);
-  }
+  await app.listen(process.env.PORT ?? 5000);
 }
 
 bootstrap();
